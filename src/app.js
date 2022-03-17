75887e8d0fb448379b3a58628b765748cd4f1985
HyoJin Kim <garcia98@naver.com>
2022-03-17 22:45:37 +0900

    Initial setup

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const { App } = require("@slack/bolt");
+require("dotenv").config();
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+app.command("/knowledge", async ({ command, ack, say }) => {
+  try {
+    await ack();
+    await say("Knowledge Search!");
+  } catch (error) {
+    console.log("err");
+    console.error(error);
+  }
+});
+
+(async () => {
+  const port = 3000;
+  // Start your app
+  await app.start(process.env.PORT || port);
+  console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+})();
